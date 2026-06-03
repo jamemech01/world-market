da4c9f12187efe472c19a64c7606b36f9098c720
Jamemech <jamemech01@gmail.com>
2026-06-03 16:04:13 +0700

    fix: prisma service

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/scripts/default-index.js';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
